bd959d6e7c4388d40876ed1f7b08dd93ff5bd2c0
Matthew <m.mccoach88@gmail.com>
2025-11-28 12:54:54 +0000

    MMD-45 : MINOR : Skeleton and render a navbar component.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './views/Navbar.js'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <p>
           Project Pokemon
         </p>
+        <Navbar/>
       </header>
     </div>
   );
---
+++ src/views/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+    render() {
+        return <div>I'm a navbar</div>
+    }
+}
+
+export default Navbar;
