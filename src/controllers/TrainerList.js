c4ece95f5a9a4e49493a9bb68781b4b4de600af3
Matthew <m.mccoach88@gmail.com>
2025-12-09 16:28:20 +0000

    MMD-66 : MINOR : Add preloader component and css.

--- src/controllers/TrainerList.js
+++ src/controllers/TrainerList.js
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 
 
 function TrainerList () {
-    
        
     const [trainers, setTrainers] = useState([]);
     const [loaded, setLoaded] = useState(false);
